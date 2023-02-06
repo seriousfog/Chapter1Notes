2e1cf61dfe0017b6c14b79fca4be9eb8e2c6acaf
t9uo <t9uo@fresnou.org>
2023-02-06 09:41:49 -0800

    initial commit

---
+++ DataTypes.js
@@ -0,0 +1,24 @@
+console.log(1);
+//This is a comment
+
+//Numbers
+//Intergers
+console.log(42);
+console.log(3);
+console.log(-2);
+//Floating Points Number
+console.log(312.2);
+console.log(92.1);
+
+//Strings
+//Characters, letters, symbols, spaces
+console.log("heello sword");
+console.log('hahaha hahaha');
+console.log('hashaahha asdas 123');
+
+console.log('12+5 = ${12+5}')
+
+//Concatenation: combine strings
+console.log("My name"+" is ms bolin")
+console.log('200 divided by 8 equals ${200/8}')
+console.log("Your age \n is 14")
---
+++ Operator.js
@@ -0,0 +1,41 @@
+console.log(typeof 15);
+console.log(typeof'this');
+console.log(typeof "that");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof"TRUE");
+
+console.log( 7 == 7 );
+console.log( 7 != 7 );
+console.log( 7 == 10 );
+console.log( 7 != 10 );
+console.log( 15 > 10 );
+console.log( 21 > 30 );
+console.log( 15 > 15);
+console.log( 15 >= 15);
+console.log( 15 <= 15);
+console.log( 15 < 15);
+console.log( 15 < 20);
+console.log( 15 < 20);
+console.log( 21 <= 50);
+console.log( 23 <= 10 );
+
+console.log( 1 > 0 && 10 <= 100);
+console.log( 1 > 0 && 10 >= 100);
+console.log( 1 < 0 && 10 == 100);
+console.log( 1 > 0 || 10 <= 100);
+console.log( 1 > 0  || 10 >= 100);
+console.log( 1 < 0 || 10 == 100);
+
+console.log( 1 < 5 ? "a" : "b" );
+console.log(1>5 ? "yes" : "no")
+console.log(1>5 ? 1:20)
+console.log(21 != 20 ? "you win!" : "you lose!")
+
+console.log(1+1 ==2 && 10 * 10 > 50)
+console.log(70%25)
+console.log("Hello" + "\nWorld!")
+console.log('17 + 2 = ${17+2}')
+console.log(25>100 && 25>10)
+console.log(25>100 || 25>10)
+console.log(3*2>1 || 25+71 != 17%5)
